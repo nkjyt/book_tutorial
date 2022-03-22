28c525db24666000e2d28b7406afc2cfd8957f75
yuta_ubuntu <nkjmyut0511@gmail.com>
2022-03-22 18:52:19 +0900

    step1の実装

---
+++ front/src/App.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./App.css";
+import { BookToRead } from "./BookToRead";
+import BookRow from "./BookRow";
+import { idText } from "typescript";
+
+const dummyBooks: BookToRead[] = [
+  {
+    id: 1,
+    title: "はじめてのReact",
+    authors: "ダミー",
+    memo: ""
+  },
+  {
+    id: 2,
+    title: "React Hooks入門",
+    authors: "ダミー",
+    memo: ""
+  },
+  {
+    id: 3,
+    title: "実践Reactアプリケーション開発",
+    authors: "ダミー",
+    memo: ""
+  }
+];
+
+const App = () => {
+  const bookRows = dummyBooks.map((b) => {
+    return (
+      <BookRow
+        book={b}
+        key={b.id}
+        onMemoChange={(id, memo) => {}}
+        onDelete={(id) => {}}
+      />
+    );
+  });
+  return (
+    <div className="App">
+      <section className="nav">
+        <h1>読みたい本リスト</h1>
+        <div className="button-like">本を追加</div>
+      </section>
+      <section className="main">
+        {bookRows}
+      </section>
+    </div>
+  );
+};
+
+export default App;
